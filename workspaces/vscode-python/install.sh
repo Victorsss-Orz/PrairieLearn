21cb460d7b9199ef3dbdaa5043acee7a6ee7ea4a
Victor Zhao <victorsss.orz@outlook.com>
2024-03-04 16:07:05 -0600

    installing with pip directly but failed

---
+++ workspaces/vscode-python/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+# Install dependencies and various libraries
+apt-get update && apt-get -y upgrade
+apt-get -y install python3 python3-pip
+apt-get -y install gcc graphviz graphviz-dev
+
+# Install all Python dependencies.
+pip3 install -r /requirements.txt
+
+# Clear various caches to minimize the final image size.
+apt-get clean
+pip3 cache purge
+
+# Delete ourself.
+rm /requirements.txt /install.sh
